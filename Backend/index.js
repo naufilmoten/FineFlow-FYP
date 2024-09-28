7d47448f5e018c5527fb7edb182a0679dc5be801
Ahmed Baig <ahmedbaig137@gmail.com>
2024-09-29 01:45:01 +0500

    final merge

--- Backend/index.js
+++ Backend/index.js
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
-=======
 
 
->>>>>>> hammad1
 require('dotenv').config(); // Load environment variables from .env file
 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
-=======
 const cors = require('cors'); // Import the CORS middleware
->>>>>>> hammad1
 const registerRoute = require('./routes/registrationRoutes'); // Import the registration routes
 const wardenRoute = require('./routes/wardenRoutes'); // Import the warden routes
 const citizenRoute = require('./routes/citizenRoutes'); // Import the citizen routes
 
 const app = express();
-<<<<<<< HEAD
-const port = process.env.PORT || 3000; // Use PORT from .env file or default to 3000
-=======
 const port = process.env.PORT || 5000; // Use PORT from .env file or default to 5000
->>>>>>> hammad1
 const dbURI = process.env.DB_URI; // Use DB_URI from .env file
 
 // Connect to MongoDB
@@ -39,8 +29,6 @@ mongoose.connect(dbURI, {
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
-=======
 // Enable CORS for all routes
 app.use(cors({
     origin: 'http://localhost:3000', // Allow requests from this origin (your frontend)
@@ -48,7 +36,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'] // Specify the allowed headers
 }));
 
->>>>>>> hammad1
 // Use the registration routes
 app.use('/api/registration', registerRoute);
 
