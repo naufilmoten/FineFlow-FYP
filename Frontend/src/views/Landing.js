6b70603ac2c71e62e7261b324a1584278a3e3a69
hamadhk7 <hammadhk17172gmail.com>
2024-10-03 06:36:42 +0500

    eleventh commit

--- Frontend/src/views/Landing.js
+++ Frontend/src/views/Landing.js
@@ -12,13 +12,25 @@ export default function Landing() {
       <Navbar transparent />
       <main>
         <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
-          <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{
-              backgroundImage:
-                "url('https://unsplash.com/photos/aerial-photography-of-concrete-roads-7nrsVjvALnA')",
-            }}
-          >
+        <div
+  className="absolute top-0 w-full h-full bg-center bg-cover"
+  style={{
+    backgroundImage:
+      "url('https://unsplash.com/photos/aerial-photography-of-concrete-roads-7nrsVjvALnA')",
+  }}
+>
+  <video
+    className="absolute top-0 left-0 w-full h-full object-cover"
+    autoPlay
+    loop
+    muted
+    playsInline
+  >
+    <source src="assets/img/traffic.mp4" type="video/mp4" />
+    
+  </video>
+</div>
+
             <span
               id="blackOverlay"
               className="w-full h-full absolute opacity-75 bg-black"
@@ -28,7 +40,11 @@ export default function Landing() {
             <div className="items-center flex flex-wrap">
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
               <div className="pr-12">
-  <h1 className="text-white font-semibold text-5xl whitespace-nowrap overflow-hidden">
+     
+     
+     
+     
+     <h1 className="text-white font-semibold text-5xl whitespace-nowrap overflow-hidden">
     🚦 Future of Payments 🚦
   </h1>
 </div>
